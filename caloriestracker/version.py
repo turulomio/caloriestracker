490d26441caa1fbf271e7a6b038a5bd0a1d8dde5
turulomio <turulomio@yahoo.es>
2019-12-08 20:36:51 +0100

    Added view allproducts

--- caloriestracker/version.py
+++ caloriestracker/version.py
@@ -1,5 +1,5 @@
 ## @brief Package with package version information
 from datetime import datetime
 
-__version__ ='0.5.0'
-__versiondatetime__=datetime(2019, 12, 8, 19, 46)
+__version__ ='0.5.9999'
+__versiondatetime__=datetime(2019, 12, 8, 20, 32)
