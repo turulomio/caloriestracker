71757040df48bfd746ca2ce9dd4cba8dabbe1a3c
turulomio <turulomio@yahoo.es>
2020-01-29 19:01:24 +0100

    Added two products additives

---
+++ caloriestracker/sql/202001291844.sql
@@ -0,0 +1,3 @@
+-- Adding two more additiverisks
+UPDATE public.products SET additives='{94}' WHERE id=53; -- Tomate frito
+UPDATE public.products SET additives='{209,280}' WHERE id=75; -- Leche evaporada
