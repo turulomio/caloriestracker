a3d7c40f56047f41bdc55270a4d3f39d2b1442d6
turulomio <turulomio@yahoo.es>
2021-03-21 20:44:37 +0100

    Added developing version

--- caloriestracker/version.py
+++ caloriestracker/version.py
@@ -1,5 +1,5 @@
 ## @brief Package with package version information
 from datetime import datetime
 
-__version__ ='0.7.0'
-__versiondatetime__=datetime(2021, 3, 21, 12, 11)
+__version__ ='0.7.9999'
+__versiondatetime__=datetime(2021, 3, 21, 20, 44)
