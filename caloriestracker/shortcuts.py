6fe7343aebff99e76d89a9562322a5e687a9e67f
turulomio <turulomio@yahoo.es>
2021-03-23 19:29:02 +0100

    Removed caloriestracker_init shortcut

--- caloriestracker/shortcuts.py
+++ caloriestracker/shortcuts.py
@@ -23,27 +23,9 @@ def create():
     persist_file = shortcut.QueryInterface (pythoncom.IID_IPersistFile)
     persist_file.Save (os.path.join (desktop_path, "caloriestracker.lnk"), 0)
     print("A shortcut have been placed in your desktop ;)")    
-    
-    shortcut = pythoncom.CoCreateInstance (
-      shell.CLSID_ShellLink,
-      None,
-      pythoncom.CLSCTX_INPROC_SERVER,
-      shell.IID_IShellLink
-    )
-
-    icon=pkg_resources.resource_filename("caloriestracker","images/caloriestracker.ico")
-    shortcut.SetPath (r'caloriestracker_init.exe')
-    shortcut.SetDescription ("Calories tracker system database initialization")
-    shortcut.SetIconLocation (icon, 0)
-     
-    desktop_path = shell.SHGetFolderPath (0, shellcon.CSIDL_DESKTOP, 0, 0)
-    persist_file = shortcut.QueryInterface (pythoncom.IID_IPersistFile)
-    persist_file.Save (os.path.join (desktop_path, "caloriestracker_init.lnk"), 0)
-    print("A shortcut have been placed in your desktop ;)")
 
 
 def remove():
     desktop_path = shell.SHGetFolderPath (0, shellcon.CSIDL_DESKTOP, 0, 0)
     os.remove(os.path.join (desktop_path, "caloriestracker.lnk"))
-    os.remove(os.path.join (desktop_path, "caloriestracker_init.lnk"))
 
